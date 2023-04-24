eab7c1c2a65b0a10b43c0cfe65563ff923aecca3
Amanda HT <amandaht99@gmail.com>
2023-04-24 22:57:35 +0200

    Secret Key fixed

--- app/app.py
+++ app/app.py
@@ -2,12 +2,14 @@ from . import cookies
 from flask import Flask, redirect, url_for, render_template
 from app.extensions.database import db, migrate
 from app.extensions.authentication import login_manager
+from os import environ
+
 
 def create_app():
   app = Flask(__name__)
-  # app.config.from_object('app.config')
-  app.secret_key = "HsfQThoXlTn9F3Y3UGTkkZEnLY5vpZ2a"
-  app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+  app.secret_key = environ.get('SECRET_KEY')
+  app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL')
+
 
   register_extensions(app)
   register_blueprints(app)
--- app/config.py
+++ app/config.py
@@ -3,10 +3,7 @@ from dotenv import load_dotenv
 
 TOPICS_PER_PAGE = 5
 SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
-#SECRET_KEY = environ.get('SECRET_KEY')
-SECRET_KEY = "HsfQThoXlTn9F3Y3UGTkkZEnLY5vpZ2a"
-
-print("test", SECRET_KEY)
+SECRET_KEY = environ.get('SECRET_KEY')
 
 load_dotenv()
 
