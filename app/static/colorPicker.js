7470c0c1147fdbb0b223801932a693747d693294
Amanda HT <amandaht99@gmail.com>
2023-04-25 20:34:40 +0200

    Added JS colorpicker

---
+++ app/static/colorPicker.js
@@ -0,0 +1,19 @@
+const colorPickerGreen = document.getElementById("color-picker-green"); 
+colorPickerGreen.addEventListener("click", function changeColor(event){
+    document.body.style.backgroundColor = "#a9fda94f"
+});
+
+const colorPickerRed = document.getElementById("color-picker-red"); 
+colorPickerRed.addEventListener("click", function changeColor(event){
+    document.body.style.backgroundColor = "#fda9a94f"
+});
+
+const colorPickerBlue = document.getElementById("color-picker-blue"); 
+colorPickerBlue.addEventListener("click", function changeColor(event){
+    document.body.style.backgroundColor = "#a9d4fd4f"
+});
+
+const colorPickerYellow = document.getElementById("color-picker-yellow"); 
+colorPickerYellow.addEventListener("click", function changeColor(event){
+    document.body.style.backgroundColor = "#fafda94f"
+});
